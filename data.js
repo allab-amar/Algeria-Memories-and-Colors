e4ee5f81e14aae2328cdd5ec94053ccf13e96b52
allabamar29121973-hub <allabamar29121973@gmail.com>
2026-09-03 21:00:03 +0200

    Add files via upload

---
+++ data.js
@@ -0,0 +1,5 @@
+const ARCHIVE = [
+  {name:"BECHAN Mohamed",arabic_name:"محمد بشان",document:"§ A.3°",image:"images/BECHAN Mohamed.jpg",keywords:"bechan mohamed محمد بشان"},
+  {name:"BEDDAR Mohamed",arabic_name:"محمد بدار",document:"§ C.1°",image:"images/BEDDAR Mohamed.jpg",keywords:"beddar mohamed محمد بدار"},
+  {name:"BEKKAI Ben Mohamed",arabic_name:"بكاي بن محمد",document:"§ C.1°",image:"images/BEKKAI Ben Mohamed.jpg",keywords:"bekkai ben mohamed بكاي بن محمد"}
+];
