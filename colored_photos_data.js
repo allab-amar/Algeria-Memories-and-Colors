601c2f1a904647811422cc943337dc89d2366539
amar <allabamar29121973@gmail.com>
2026-09-09 22:07:47 +0200

    Add files via upload

--- colored_photos_data.js
+++ colored_photos_data.js
@@ -1,13 +1,18 @@
-// قاعدة بيانات الصور الملونة التاريخية (Algeria Memories and Colors)
 window.COLORED_PHOTOS_ARCHIVE = [
-  /* نموذج لصورة ملونة:
   {
-    "id": "col_1",
-    "title": "مشهد ملون من القصبة العتيقة",
-    "image": "images/example_colored.jpg",
-    "year": "1958",
-    "place": "الجزائر العاصمة",
+    "id": "col_1788983320847",
+    "title": "الشهيد فوضيل احمد",
+    "image": "images/col_1788983320328.jpg",
+    "year": "أكتوبر 1957",
+    "place": "",
+    "dateAdded": "2026-09-09"
+  },
+  {
+    "id": "col_1788984305791",
+    "title": "BENASSI Mahmoud et YAHIAOUI Tedjini",
+    "image": "images/col_1788984305283.jpg",
+    "year": "",
+    "place": "التُقطت هذه الصورة قبيل إعدامهما بعد أن تعرّضا لأبشع أنواع التعذيب في عين الصفراء.",
     "dateAdded": "2026-09-09"
   }
-  */
 ];
--- slna_cards_data.js
+++ slna_cards_data.js
@@ -1,14 +1,20 @@
-// قاعدة بيانات بطاقات استعلامات مصلحة الاتصال لشمال إفريقيا (SLNA - 1957)
 window.SLNA_CARDS_ARCHIVE = [
-  /* نموذج لبطاقة استعلامات:
   {
-    "id": "slna_1",
-    "personName": "AUDIN Maurice",
-    "cardImage": "images/slna_audin.jpg",
-    "disappearanceDate": "1957-06-11",
-    "details": "عالم رياضيات ومساعد في كلية العلوم بالجزائر، اعتقل من منزله بأبيار ولم يظهر له أثر.",
+    "id": "slna_1788983487032",
+    "personName": "Bouchicha   youcef   ben  ali",
+    "cardImage": "documents_images/slna_1788983486939.jpg",
+    "disappearanceDate": "714-12-195",
+    "details": "Cite la Montagne Bloc II B/16 Hussein-DEy",
+    "source": "1000autres.org / SLNA",
+    "dateAdded": "2026-09-09"
+  },
+  {
+    "id": "slna_1788984219072",
+    "personName": "Mohamed Larbi Soual Ben Rabah",
+    "cardImage": "documents_images/slna_1788984219001.jpg",
+    "disappearanceDate": "25-121957",
+    "details": "Cite des Eucalyptus - Bt B- H° 3 - Hussein-Dey",
     "source": "1000autres.org / SLNA",
     "dateAdded": "2026-09-09"
   }
-  */
 ];
