1f5172329573828d7e051f3523e1cd022e8ad1ff
amar <allabamar29121973@gmail.com>
2026-09-09 22:41:04 +0200

    Add files via upload

--- colored_photos_data.js
+++ colored_photos_data.js
@@ -2,7 +2,7 @@ window.COLORED_PHOTOS_ARCHIVE = [
   {
     "id": "col_1788983320847",
     "title": "الشهيد فوضيل احمد",
-    "image": "images/col_1788983320328.jpg",
+    "image": "colored_images/col_1788983320328.jpg",
     "year": "أكتوبر 1957",
     "place": "",
     "dateAdded": "2026-09-09"
@@ -10,7 +10,7 @@ window.COLORED_PHOTOS_ARCHIVE = [
   {
     "id": "col_1788984305791",
     "title": "BENASSI Mahmoud et YAHIAOUI Tedjini",
-    "image": "images/col_1788984305283.jpg",
+    "image": "colored_images/col_1788984305283.jpg",
     "year": "",
     "place": "التُقطت هذه الصورة قبيل إعدامهما بعد أن تعرّضا لأبشع أنواع التعذيب في عين الصفراء.",
     "dateAdded": "2026-09-09"
--- slna_cards_data.js
+++ slna_cards_data.js
@@ -2,7 +2,7 @@ window.SLNA_CARDS_ARCHIVE = [
   {
     "id": "slna_1788983487032",
     "personName": "Bouchicha   youcef   ben  ali",
-    "cardImage": "documents_images/slna_1788983486939.jpg",
+    "cardImage": "slna_images/slna_1788983486939.jpg",
     "disappearanceDate": "714-12-195",
     "details": "Cite la Montagne Bloc II B/16 Hussein-DEy",
     "source": "1000autres.org / SLNA",
@@ -11,7 +11,7 @@ window.SLNA_CARDS_ARCHIVE = [
   {
     "id": "slna_1788984219072",
     "personName": "Mohamed Larbi Soual Ben Rabah",
-    "cardImage": "documents_images/slna_1788984219001.jpg",
+    "cardImage": "slna_images/slna_1788984219001.jpg",
     "disappearanceDate": "25-121957",
     "details": "Cite des Eucalyptus - Bt B- H° 3 - Hussein-Dey",
     "source": "1000autres.org / SLNA",
