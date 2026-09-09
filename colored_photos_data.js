87f43cdad1d33b4ca99accfb44d3e28dee147958
amar <allabamar29121973@gmail.com>
2026-09-09 21:45:27 +0200

    Add files via upload

---
+++ colored_photos_data.js
@@ -0,0 +1,13 @@
+// قاعدة بيانات الصور الملونة التاريخية (Algeria Memories and Colors)
+window.COLORED_PHOTOS_ARCHIVE = [
+  /* نموذج لصورة ملونة:
+  {
+    "id": "col_1",
+    "title": "مشهد ملون من القصبة العتيقة",
+    "image": "images/example_colored.jpg",
+    "year": "1958",
+    "place": "الجزائر العاصمة",
+    "dateAdded": "2026-09-09"
+  }
+  */
+];
---
+++ slna_cards_data.js
@@ -0,0 +1,14 @@
+// قاعدة بيانات بطاقات استعلامات مصلحة الاتصال لشمال إفريقيا (SLNA - 1957)
+window.SLNA_CARDS_ARCHIVE = [
+  /* نموذج لبطاقة استعلامات:
+  {
+    "id": "slna_1",
+    "personName": "AUDIN Maurice",
+    "cardImage": "images/slna_audin.jpg",
+    "disappearanceDate": "1957-06-11",
+    "details": "عالم رياضيات ومساعد في كلية العلوم بالجزائر، اعتقل من منزله بأبيار ولم يظهر له أثر.",
+    "source": "1000autres.org / SLNA",
+    "dateAdded": "2026-09-09"
+  }
+  */
+];
