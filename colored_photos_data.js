f9add0cb3e468ce75f050460ad23e64af2356e65
amar <allabamar29121973@gmail.com>
2026-09-10 20:38:18 +0200

    Add files via upload

--- colored_photos_data.js
+++ colored_photos_data.js
@@ -14,5 +14,85 @@ window.COLORED_PHOTOS_ARCHIVE = [
     "year": "",
     "place": "التُقطت هذه الصورة قبيل إعدامهما بعد أن تعرّضا لأبشع أنواع التعذيب في عين الصفراء.",
     "dateAdded": "2026-09-09"
+  },
+  {
+    "id": "col_1789064942437",
+    "title": "المجاهد مازوني عبد العزيز المدعو التونسي .رحمه الله .",
+    "image": "colored_images/col_1789064942348.jpg",
+    "year": "جوان 1959",
+    "place": "الحساسنة ولاية سعيدة",
+    "dateAdded": "2026-09-10"
+  },
+  {
+    "id": "col_1789065027151",
+    "title": "L’un des moudjahidines tombés dans les montagnes de Saïda aux mains des unités « Georges » et « Maurice  ,",
+    "image": "colored_images/col_1789065027078.jpg",
+    "year": "avril 1959",
+    "place": "Saïda",
+    "dateAdded": "2026-09-10"
+  },
+  {
+    "id": "col_1789065067095",
+    "title": "Saïda 1959",
+    "image": "colored_images/col_1789065067015.jpg",
+    "year": "1959",
+    "place": "Saïda",
+    "dateAdded": "2026-09-10"
+  },
+  {
+    "id": "col_1789065160752",
+    "title": "Cheikh Lachbor, alias Mohamed Ben Cheikh    Naamaoui Fatma, alias « Djamila »",
+    "image": "colored_images/col_1789065160689.jpg",
+    "year": "07/12/1959",
+    "place": "Saïda  07/12/1959",
+    "dateAdded": "2026-09-10"
+  },
+  {
+    "id": "col_1789065213027",
+    "title": "جامع سيدي بن علي",
+    "image": "colored_images/col_1789065212954.jpg",
+    "year": "",
+    "place": "",
+    "dateAdded": "2026-09-10"
+  },
+  {
+    "id": "col_1789065229176",
+    "title": "جامع سيدي بن علي",
+    "image": "colored_images/col_1789065229035.jpg",
+    "year": "",
+    "place": "",
+    "dateAdded": "2026-09-10"
+  },
+  {
+    "id": "col_1789065267968",
+    "title": "Le colonel Amirouche",
+    "image": "colored_images/col_1789065267900.jpg",
+    "year": "",
+    "place": "",
+    "dateAdded": "2026-09-10"
+  },
+  {
+    "id": "col_1789065314816",
+    "title": "الأمير عبد القادر",
+    "image": "colored_images/col_1789065314749.jpg",
+    "year": "",
+    "place": "",
+    "dateAdded": "2026-09-10"
+  },
+  {
+    "id": "col_1789065351866",
+    "title": "الحراش  1962",
+    "image": "colored_images/col_1789065351798.jpg",
+    "year": "",
+    "place": "",
+    "dateAdded": "2026-09-10"
+  },
+  {
+    "id": "col_1789065427352",
+    "title": "Embuscade par la 8e compagnie de Harka. ربي يرحمهم… لقد تعرّضوا لتمثيل بشع",
+    "image": "colored_images/col_1789065427264.jpg",
+    "year": "",
+    "place": "",
+    "dateAdded": "2026-09-10"
   }
 ];
