732d2b07efa3f2240e153d81fbe17da8776782c0
amar <allabamar29121973@gmail.com>
2026-09-11 19:59:42 +0200

    Add files via upload

--- documents_data.js
+++ documents_data.js
@@ -1,1 +1,112 @@
-const DOCUMENTS_ARCHIVE = [];
+const DOCUMENTS_ARCHIVE = [
+  {
+    "document_title": "ياسف سعدي",
+    "image": "documents_images/doc_1789148581909_0.jpg",
+    "images": [
+      "documents_images/doc_1789148581909_0.jpg",
+      "documents_images/doc_1789148582042_1.jpg",
+      "documents_images/doc_1789148582111_2.jpg",
+      "documents_images/doc_1789148582182_3.jpg",
+      "documents_images/doc_1789148582252_4.jpg",
+      "documents_images/doc_1789148582321_5.jpg",
+      "documents_images/doc_1789148582393_6.jpg",
+      "documents_images/doc_1789148582462_7.jpg",
+      "documents_images/doc_1789148582535_8.jpg"
+    ],
+    "names": [
+      "ياسف سعدي"
+    ],
+    "pageNames": [
+      [],
+      [],
+      [],
+      [],
+      [],
+      [],
+      [],
+      [],
+      []
+    ],
+    "year": "",
+    "place": ""
+  },
+  {
+    "document_title": "الشهيد سي الحواس",
+    "image": "documents_images/doc_1789148726234_0.jpg",
+    "images": [
+      "documents_images/doc_1789148726234_0.jpg",
+      "documents_images/doc_1789148726307_1.jpg",
+      "documents_images/doc_1789148726377_2.jpg",
+      "documents_images/doc_1789148726447_3.jpg",
+      "documents_images/doc_1789148726516_4.jpg"
+    ],
+    "names": [
+      "الشهيد سي الحواس"
+    ],
+    "pageNames": [
+      [],
+      [],
+      [],
+      [],
+      []
+    ],
+    "year": "",
+    "place": ""
+  },
+  {
+    "document_title": "الشهيد عبد الرحمان ميرة",
+    "image": "documents_images/doc_1789149091492_0.jpg",
+    "images": [
+      "documents_images/doc_1789149091492_0.jpg",
+      "documents_images/doc_1789149091564_1.jpg",
+      "documents_images/doc_1789149091634_2.jpg",
+      "documents_images/doc_1789149091705_3.jpg",
+      "documents_images/doc_1789149091777_4.jpg",
+      "documents_images/doc_1789149091857_5.jpg",
+      "documents_images/doc_1789149091927_6.jpg",
+      "documents_images/doc_1789149091997_7.jpg",
+      "documents_images/doc_1789149092069_8.jpg",
+      "documents_images/doc_1789149092140_9.jpg",
+      "documents_images/doc_1789149092222_10.jpg"
+    ],
+    "names": [
+      "الشهيد عبد الرحمان ميرة"
+    ],
+    "pageNames": [
+      [],
+      [],
+      [],
+      [],
+      [],
+      [],
+      [],
+      [],
+      [],
+      [],
+      []
+    ],
+    "year": "",
+    "place": ""
+  },
+  {
+    "document_title": "الشهيد بن بولعيد",
+    "image": "documents_images/doc_1789149120141_0.jpg",
+    "images": [
+      "documents_images/doc_1789149120141_0.jpg",
+      "documents_images/doc_1789149120211_1.jpg",
+      "documents_images/doc_1789149120280_2.jpg",
+      "documents_images/doc_1789149120351_3.jpg"
+    ],
+    "names": [
+      "الشهيد بن بولعيد"
+    ],
+    "pageNames": [
+      [],
+      [],
+      [],
+      []
+    ],
+    "year": "",
+    "place": ""
+  }
+];
